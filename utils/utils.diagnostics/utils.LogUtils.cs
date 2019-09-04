9d3d2b7aee3168060908a5997b30f892666f8f49
Oleksandr Hubanov <oleksandr.Hubanov@seraphimas-hls.com>
2019-09-04 16:12:26 +0300

    done with diagnostics lib

--- utils/utils.diagnostics/utils.LogUtils.cs
+++ utils/utils.diagnostics/utils.LogUtils.cs
@@ -1,222 +1,269 @@
 ﻿using System;
-using System.Text;
-using System.Threading;
 using System.Diagnostics;
-using System.Xml.Serialization;
 using System.IO;
+using System.Threading;
 
 
-namespace utils {
+namespace utils
+{
 
-	public interface ILogUtils {
-		TraceListenerCollection listeners {get;}
-		void WriteEvent(string evtMsg, string evtSrc, TraceEventType evtType);
-		void Flush();
-		void Close();
-	}
+    public interface ILogUtils
+    {
+        TraceListenerCollection listeners { get; }
+        void WriteEvent(string evtMsg, string evtSrc, TraceEventType evtType);
+        void Flush();
+        void Close();
+    }
 
-	public class LogUtilsBase : ILogUtils {
+    public class LogUtilsBase : ILogUtils
+    {
 
-		//--------------------------------------------------
-		// public section
-		//--------------------------------------------------
+        //--------------------------------------------------
+        // public section
+        //--------------------------------------------------
 
-		public virtual Guid activityId {
-			get {
-				return Guid.Empty;
-			}
-		}
+        public virtual Guid activityId
+        {
+            get
+            {
+                return Guid.Empty;
+            }
+        }
 
-		//--------------------------------------------------
-		// ILogUtils implementation
-		//--------------------------------------------------
+        //--------------------------------------------------
+        // ILogUtils implementation
+        //--------------------------------------------------
 
-		public TraceListenerCollection listeners {
-			get {
-				return Trace.Listeners;
-			}
-		}
-		
-		public void WriteEvent(string message, string source, TraceEventType type) {
-			var ctx = BeforeWriteEvent();
-			try {
-				if (source == null) {
-					source = s_processFileName;
-				}
-				var evtId = Interlocked.Increment(ref _evtId);
-				var evtCache = new TraceEventCache();
-				foreach (TraceListener l in listeners) {
-					lock (l) {
-						try {
-							l.TraceEvent(evtCache, source, type, evtId, message);
-							if (Trace.AutoFlush) {
-								l.Flush();
-							}
-						} catch {
-							//swallow error
-							Debugger.Break();
-						}
-					}
-				}
-			} finally {
-				AfterWriteEvent(ctx);
-			}
-		}
+        public TraceListenerCollection listeners
+        {
+            get
+            {
+                return Trace.Listeners;
+            }
+        }
 
-		public void Flush() {
-			foreach (TraceListener l in listeners) {
-				lock (l) {
-					l.Flush();
-				}
-			}
-		}
-		public void Close() {
-			foreach (TraceListener l in listeners) {
-				lock (l) {
-					l.Close();
-				}
-			}
-		}
-		
-		//--------------------------------------------------
-		// protected section
-		//--------------------------------------------------
+        public void WriteEvent(string message, string source, TraceEventType type)
+        {
+            var ctx = BeforeWriteEvent();
+            try
+            {
+                if (source == null)
+                {
+                    source = s_processFileName;
+                }
+                var evtId = Interlocked.Increment(ref _evtId);
+                var evtCache = new TraceEventCache();
+                foreach (TraceListener l in listeners)
+                {
+                    lock (l)
+                    {
+                        try
+                        {
+                            l.TraceEvent(evtCache, source, type, evtId, message);
+                            if (Trace.AutoFlush)
+                            {
+                                l.Flush();
+                            }
+                        }
+                        catch
+                        {
+                            //swallow error
+                            Debugger.Break();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                AfterWriteEvent(ctx);
+            }
+        }
 
-		protected static string s_processFileNameField = null;
-		protected static string s_processFileName {
-			get {
-				if (s_processFileNameField == null) {
-					s_processFileNameField = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
-				}
-				return s_processFileNameField;
-			}
-		}
+        public void Flush()
+        {
+            foreach (TraceListener l in listeners)
+            {
+                lock (l)
+                {
+                    l.Flush();
+                }
+            }
+        }
+        public void Close()
+        {
+            foreach (TraceListener l in listeners)
+            {
+                lock (l)
+                {
+                    l.Close();
+                }
+            }
+        }
 
-		protected virtual Object BeforeWriteEvent() {
-			var oldActivityId = Trace.CorrelationManager.ActivityId;
-			if (oldActivityId != activityId) {
-				Trace.CorrelationManager.ActivityId = activityId;
-			}
-			return oldActivityId;
-		}
+        //--------------------------------------------------
+        // protected section
+        //--------------------------------------------------
 
-		protected virtual void AfterWriteEvent(Object context) {
-			var oldActivityId = (Guid)context;
-			if (oldActivityId != activityId) {
-				Trace.CorrelationManager.ActivityId = activityId;
-			}
-		}
+        protected static string s_processFileNameField = null;
+        protected static string s_processFileName
+        {
+            get
+            {
+                if (s_processFileNameField == null)
+                {
+                    s_processFileNameField = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                }
+                return s_processFileNameField;
+            }
+        }
 
-		//--------------------------------------------------
-		// private section
-		//--------------------------------------------------
+        protected virtual Object BeforeWriteEvent()
+        {
+            var oldActivityId = Trace.CorrelationManager.ActivityId;
+            if (oldActivityId != activityId)
+            {
+                Trace.CorrelationManager.ActivityId = activityId;
+            }
+            return oldActivityId;
+        }
 
-		private int _evtId = 0;
-	}
+        protected virtual void AfterWriteEvent(Object context)
+        {
+            var oldActivityId = (Guid)context;
+            if (oldActivityId != activityId)
+            {
+                Trace.CorrelationManager.ActivityId = activityId;
+            }
+        }
+
+        //--------------------------------------------------
+        // private section
+        //--------------------------------------------------
+
+        private int _evtId = 0;
+    }
 
 
-	public class LogUtilsDefault : LogUtilsBase {
+    public class LogUtilsDefault : LogUtilsBase
+    {
 
-		//--------------------------------------------------
-		// public section
-		//--------------------------------------------------
-		
-		public LogUtilsDefault() {
-			_activityId = Guid.NewGuid();
-		}
+        //--------------------------------------------------
+        // public section
+        //--------------------------------------------------
 
-		public LogUtilsDefault(Guid activityId) {
-			this._activityId = activityId;
-		}
+        public LogUtilsDefault()
+        {
+            _activityId = Guid.NewGuid();
+        }
 
-		//--------------------------------------------------
-		// protected section
-		//--------------------------------------------------
+        public LogUtilsDefault(Guid activityId)
+        {
+            this._activityId = activityId;
+        }
 
-		protected Guid _activityId;
-	}
+        //--------------------------------------------------
+        // protected section
+        //--------------------------------------------------
 
-	public class log {
+        protected Guid _activityId;
+    }
 
-		//--------------------------------------------------
-		// public section
-		//--------------------------------------------------
-		
-		static log() {
-			Init(new LogUtilsDefault());
-		}
+    public class log
+    {
 
-		[Conditional("TRACE")]
-		public static void Init(ILogUtils impl) {
-			//lock (_staticLock) {
-			log._impl = impl;
-			//}
-		}
+        //--------------------------------------------------
+        // public section
+        //--------------------------------------------------
 
-		[Conditional("TRACE")]
-		public static void WriteEvent(string evtMsg, string evtSrc, TraceEventType evtType) {
-			_impl.WriteEvent(evtMsg, evtSrc, evtType);
-		}
+        static log()
+        {
+            Init(new LogUtilsDefault());
+        }
 
-		[Conditional("TRACE")]
-		public static void WriteError(string errMsg, string source) {
-			WriteEvent(errMsg, source, TraceEventType.Error);
-		}
+        [Conditional("TRACE")]
+        public static void Init(ILogUtils impl)
+        {
+            //lock (_staticLock) {
+            log._impl = impl;
+            //}
+        }
 
-		[Conditional("TRACE")]
-		public static void WriteError(string errMsg) {
-			WriteEvent(errMsg, null, TraceEventType.Error);
-		}
+        [Conditional("TRACE")]
+        public static void WriteEvent(string evtMsg, string evtSrc, TraceEventType evtType)
+        {
+            _impl.WriteEvent(evtMsg, evtSrc, evtType);
+        }
 
-		[Conditional("TRACE")]
-		public static void WriteError(Exception err) {
+        [Conditional("TRACE")]
+        public static void WriteError(string errMsg, string source)
+        {
+            WriteEvent(errMsg, source, TraceEventType.Error);
+        }
+
+        [Conditional("TRACE")]
+        public static void WriteError(string errMsg)
+        {
+            WriteEvent(errMsg, null, TraceEventType.Error);
+        }
+
+        [Conditional("TRACE")]
+        public static void WriteError(Exception err)
+        {
 
             WriteEvent(GetInner(err), null, TraceEventType.Error);
-		}
-        static string GetInner(Exception err) {
+        }
+        static string GetInner(Exception err)
+        {
             string result = err.ToString();
-            if (err.InnerException != null) {
+            if (err.InnerException != null)
+            {
                 result += Environment.NewLine + GetInner(err.InnerException);
             }
             return result;
         }
-		
-		[Conditional("TRACE")]
-		public static void WriteWarning(string warnMsg, string source) {
-			WriteEvent(warnMsg, source, TraceEventType.Warning);
-		}
 
-		[Conditional("TRACE")]
-		public static void WriteWarning(string warnMsg) {
-			WriteEvent(warnMsg, null, TraceEventType.Warning);
-		}
+        [Conditional("TRACE")]
+        public static void WriteWarning(string warnMsg, string source)
+        {
+            WriteEvent(warnMsg, source, TraceEventType.Warning);
+        }
 
-		[Conditional("TRACE")]
-		public static void WriteInfo(string infoMsg, string source) {
-			WriteEvent(infoMsg, source, TraceEventType.Information);
-		}
+        [Conditional("TRACE")]
+        public static void WriteWarning(string warnMsg)
+        {
+            WriteEvent(warnMsg, null, TraceEventType.Warning);
+        }
 
-		[Conditional("TRACE")]
-		public static void WriteInfo(string infoMsg) {
-			WriteEvent(infoMsg, null, TraceEventType.Information);
-		}
+        [Conditional("TRACE")]
+        public static void WriteInfo(string infoMsg, string source)
+        {
+            WriteEvent(infoMsg, source, TraceEventType.Information);
+        }
 
-		[Conditional("TRACE")]
-		public static void Flush() {
-			_impl.Flush();
-		}
+        [Conditional("TRACE")]
+        public static void WriteInfo(string infoMsg)
+        {
+            WriteEvent(infoMsg, null, TraceEventType.Information);
+        }
 
-		[Conditional("TRACE")]
-		public static void Close() {
-			_impl.Close();
-		}
+        [Conditional("TRACE")]
+        public static void Flush()
+        {
+            _impl.Flush();
+        }
 
-		//--------------------------------------------------
-		// private section
-		//--------------------------------------------------
+        [Conditional("TRACE")]
+        public static void Close()
+        {
+            _impl.Close();
+        }
 
-		//private static object _staticLock = new Object();
-		private static ILogUtils _impl;
+        //--------------------------------------------------
+        // private section
+        //--------------------------------------------------
 
-	}
+        //private static object _staticLock = new Object();
+        private static ILogUtils _impl;
+
+    }
 }
