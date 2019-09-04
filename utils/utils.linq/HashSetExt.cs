f54ccc49bfbc19c1657c98e412eceec0d4bc7d9f
Oleksandr Hubanov <oleksandr.Hubanov@seraphimas-hls.com>
2019-09-04 17:32:13 +0300

    got compiled utils.linq DISABLED Append method

--- utils/utils.linq/HashSetExt.cs
+++ utils/utils.linq/HashSetExt.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace utils {
-	public class HashSetExt<T> : HashSet<T> {
-		public bool this[T key] {
-			get {
-				return Contains(key);
-			}
-			set {
-				if (value) {
-					Add(key);
-				} else {
-					Remove(key);
-				}
-			}
-		}
-	}
+namespace utils
+{
+    public class HashSetExt<T> : HashSet<T>
+    {
+        public bool this[T key]
+        {
+            get
+            {
+                return Contains(key);
+            }
+            set
+            {
+                if (value)
+                {
+                    Add(key);
+                }
+                else
+                {
+                    Remove(key);
+                }
+            }
+        }
+    }
 }
